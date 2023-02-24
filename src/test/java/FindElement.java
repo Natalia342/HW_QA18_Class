3e6f3346890de962674651b4053b9753e8a8026c
Natalia342 <nataliatalova7@gmail.com>
2023-02-25 00:48:12 +0200

    HW_6

--- src/test/java/FindElement.java
+++ src/test/java/FindElement.java
@@ -19,18 +19,49 @@ public class FindElement {
     public void testFind(){
         WebElement id = wd.findElement(By.id("root"));
         System.out.println(id);
-        List<WebElement> list = wd.findElements(By.tagName("a"));
-        System.out.println(list.size());
         WebElement text = wd.findElement(By.partialLinkText("HO"));
         System.out.println(text);
         WebElement container = wd.findElement(By.className("container"));
         System.out.println(container);
-        List<WebElement> div = wd.findElements(By.tagName("div"));
-        System.out.println(div.size());
         WebElement text2 = wd.findElement(By.linkText("ABOUT"));
         System.out.println(text2);
         List<WebElement> script = wd.findElements(By.tagName("script"));
         System.out.println(script.size());
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        System.out.println(list.size());
+        List<WebElement> div = wd.findElements(By.tagName("div"));
+        System.out.println(div.size());
+
+
+        wd.findElement(By.cssSelector("#root"));
+        wd.findElement(By.cssSelector("div#root.container"));
+        wd.findElement(By.cssSelector("div[id='root']"));
+
+        wd.findElement(By.cssSelector("div.navbar-component_nav__1X_4m>a:nth-child(2)"));
+        wd.findElement(By.cssSelector("a[href='/home']"));
+
+        wd.findElement(By.cssSelector("div.container"));
+        wd.findElement(By.cssSelector("#root.container"));
+        wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.cssSelector("div[id='root'].container"));
+        wd.findElement(By.cssSelector("div#root.container"));
+
+        wd.findElement(By.cssSelector("div.navbar-component_nav__1X_4m :nth-child(3)"));
+        wd.findElement(By.cssSelector("div a[href='/about']"));
+        wd.findElement(By.cssSelector("a[href='/about']"));
+
+        List<WebElement> script1 = wd.findElements(By.cssSelector("script"));
+        System.out.println(script1.size());
+        wd.findElement(By.cssSelector("script[src='/static/js/2.03ed4652.chunk.js']"));
+        wd.findElement(By.cssSelector("[src='/static/js/2.03ed4652.chunk.js']"));
+
+        wd.findElements(By.cssSelector("a"));
+        wd.findElements(By.cssSelector("div.navbar-component_nav__1X_4m [style]"));
+        wd.findElements(By.cssSelector("div.navbar-component_nav__1X_4m [href]"));
+
+        wd.findElements(By.cssSelector("div"));
+
+
     }
     @AfterMethod
     public void postCondition(){
---
+++ src/test/java/TableTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TableTest {
+    WebDriver wd;
+    @BeforeMethod
+    public void preCondition(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
+    }
+    @Test
+    public void tableHW2(){
+        List<WebElement> tableRows = wd.findElements(By.cssSelector("#customers>tbody>tr"));//#customers tr
+        System.out.println(tableRows.size());
+        List<WebElement> tableColumns = wd.findElements(By.cssSelector("table#customers>tbody>tr th"));//table#customers th
+        System.out.println(tableColumns.size());
+        wd.findElement(By.cssSelector("#customers td:nth-child(3)"));
+        wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
+
+    }
+    @BeforeMethod
+    public void postCondition(){
+    //    wd.quit();
+    }
+}
