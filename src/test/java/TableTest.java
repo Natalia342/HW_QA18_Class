180cba2b46cee7845f84cda2e7535e246842d000
Natalia342 <nataliatalova7@gmail.com>
2023-02-25 19:48:17 +0200

    HW_6

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -22,7 +22,7 @@ public class TableTest {
         System.out.println(tableColumns.size());
 
         wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
-        wd.findElement(By.cssSelector("#customers td:nth-child(3)"));
+        wd.findElement(By.cssSelector("table#customers>tbody>tr th:nth-child(3)"));//#customers td:nth-child(3)
     }
     @BeforeMethod
     public void postCondition(){
